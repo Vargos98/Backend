dc1e0bd4ab1e98ea3fe409e469d7c9a91ab8cd29
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-12-08 12:38:22 +0530

    Added different routes and middleware

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+app.use((req,res,next)=>{
+    console.log("Hi im the middleware");
+    next();
+})
+
+
+
 app.get('/', (req,res)=>{
 
     res.send("hello world");
 
 });
+
+app.get('/profile', (req,res)=>{
+
+    res.send("hello from profile");
+
+});
+
+app.get('/contact', (req,res)=>{
+
+    res.send("hello from contact");
+
+});
+
 
 app.listen(port);

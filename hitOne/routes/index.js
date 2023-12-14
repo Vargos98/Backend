f5474ced2d17a7f75d2e1ec8c6bb75a360a95b79
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-12-14 17:56:07 +0530

    Created MongoDB server and sent data towards it

--- hitOne/routes/index.js
+++ hitOne/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+const userModal = require("./users");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+router.get('/create', async function(req, res) {
+  const createdUser = await userModal.create({
+    username: "harsh",
+    name: "harsh",
+    age:25
+  });
+res.send(createdUser);
+
 });
 
 module.exports = router;
